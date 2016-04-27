47736d6401cf7ef4933b187bedfe92b46f4517bf
Michael Job <mj@michaeljob.ch>
2016-04-27 19:39:34 +0200

    extended nav... drafts of login / reg

--- js/nav.js
+++ js/nav.js
@@ -3,26 +3,40 @@ $(document).ready(function() {
     
 
     //Pages (section)
-    //var LOGIN = $("#login");
-   // var REG = $("#reg");
+    var LOGIN = $("#login");
+    var REG = $("#reg");
     var START = $("#start");
     var GAME = $("#game");
-  //  var SCORE = $("#score");
+    var SCORE = $("#score");
 
     //Buttons
-    //var btLogin = $("#btlogin");
-    var btPlay = $("#btplay");
+    var btLogin = $("#btLogin");
+    var btPlay = $("#btPlay");
+    var btNew = $("#btNew");
 
   	//**************Button Handler****************
 
     //Play Button
     btPlay.click(function(e){
         e.preventDefault();
-        //show map: 
         GAME.show();
         START.hide();
 
     });
 
+    //Play Button
+    btNew.click(function(e){
+        e.preventDefault();
+        REG.show();
+        LOGIN.hide();
+
+    });
+
+    //Play Button
+    btLogin.click(function(e){
+        e.preventDefault();
+        START.show();
+        LOGIN.hide();
+    });
 
 });

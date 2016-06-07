10ad79b20908709509dc38faeb64f85bb34addcd
Ina Widmer <ina.widmer@students.fhnw.ch>
2016-06-07 15:05:26 +0200

    Limit 9 for highscore

--- api/index.php
+++ api/index.php
@@ -134,7 +134,7 @@ function getHighscore() {
     $conn = getDB();
 
     //Gets correct Answers per User in %
-    $sql = "SELECT  u.username,  100/SUM(s.playedQuestions)*SUM(s.correctAnswers) as total FROM  score s, user u where s.userid=u.id GROUP BY u.id ORDER BY total DESC";
+    $sql = "SELECT  u.username,  100/SUM(s.playedQuestions)*SUM(s.correctAnswers) as total FROM  score s, user u where s.userid=u.id GROUP BY u.id ORDER BY total DESC LIMIT 9";
 
     $result = mysqli_query ($conn,$sql);
     $rows = array();

973960670ccca5f873911b792fa45ea3d5384cf7
Michael Job <mj@michaeljob.ch>
2016-06-12 12:13:31 +0200

    score without decimal

--- js/score.js
+++ js/score.js
@@ -61,7 +61,7 @@ function renderHighscoreList(data) {
 
 	//Show Highscore in Score Table
 	$.each(list, function(index, highscore) {
-		$('#score').find('tbody').append('<tr><td>'+(index+1)+'.</td><td>' + highscore.username + '</td><td>'+ Math.round(highscore.total).toFixed(2) +' %</td></tr>');
+		$('#score').find('tbody').append('<tr><td>'+(index+1)+'.</td><td>' + highscore.username + '</td><td>'+ Math.round(highscore.total).toFixed(0) +' %</td></tr>');
 	});
 }
 
